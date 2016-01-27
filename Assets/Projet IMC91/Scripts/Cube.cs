67d554c2596bf38204d7884cfe713798c9751b98
Shujin <cedric.pignard-fernandez@ensiie.fr>
2016-01-27 17:51:27 +0100

    Add target.prefab Add OnTrigger methods Update .gitignore

--- Assets/Projet IMC91/Scripts/Cube.cs
+++ Assets/Projet IMC91/Scripts/Cube.cs
@@ -3,25 +3,37 @@ using System.Collections;
 
 public class Cube : MonoBehaviour
 {
+	public bool IsHittingTarget { get; private set; }
 
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
-	
+		IsHittingTarget = false;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
 	
 	}
 
-	private void OnTriggerEnter (Collider other)
+	private void OnTriggerEnter(Collider other)
 	{
-		Debug.Log("Collision detected");
+		Debug.Log("Collision enter detected");
 		if (other.CompareTag("Target"))
 		{
 			Debug.Log("Target hit");
+			IsHittingTarget = true;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		Debug.Log("Collision exit detected");
+		if (other.CompareTag("Target"))
+		{
+			Debug.Log("Target exit");
+			IsHittingTarget = false;
 		}
 	}
 }
