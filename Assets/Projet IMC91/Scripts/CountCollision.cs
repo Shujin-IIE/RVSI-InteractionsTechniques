4779348f98ff93401940e442ce14856f83d9e345
Shujin <cedric.pignard-fernandez@ensiie.fr>
2016-02-02 09:10:12 +0100

    Add walls

---
+++ Assets/Projet IMC91/Scripts/CountCollision.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountCollision : MonoBehaviour {
+	
+	public int countCollisions = 0;
+	bool colourChangeCollision = false;
+	public float colourChangeDelay = 0.5f;
+	float currentDelay = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("im=" + this.name + " col=" + countCollisions);
+		checkColourChange();
+	}
+
+
+
+	void OnCollisionEnter(Collision hit)
+	{
+		if (hit.gameObject.name == "Cube" ){
+			//Debug.Log("one");
+			countCollisions++;
+			colourChangeCollision = true;
+			currentDelay = Time.time + colourChangeDelay;
+		}
+	}
+	
+	void checkColourChange()
+	{        
+		if(colourChangeCollision)
+		{
+			transform.GetComponent<Renderer>().material.color = Color.red;
+			if(Time.time > currentDelay)
+			{
+				transform.GetComponent<Renderer>().material.color = Color.white;
+				colourChangeCollision = false;
+			}
+		}
+	}
+
+	int GetCountCollision(){
+		return countCollisions;
+	}
+}
+	
