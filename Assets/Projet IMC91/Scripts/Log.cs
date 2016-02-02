0d3841521e96fb381d81c459ced916c6a51b2193
Mathilde GILBERT <mathilde.gilbert@ensiie.fr>
2016-02-02 10:23:37 +0100

    Modification du fichier log

--- Assets/Projet IMC91/Scripts/Log.cs
+++ Assets/Projet IMC91/Scripts/Log.cs
@@ -1,5 +1,6 @@
 ﻿using System;// Date
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;//Input Ouput
 using UnityEngine;
 
@@ -10,23 +11,28 @@ using UnityEngine;
 /// </summary>
 public class Log : MonoBehaviour
 {
-    public String fileName = "fileName.csv";
+    public String fileName = "fileName";
     public float timeSample = 0.5f;
     private StreamWriter stream;
+	private Timer Timer;
+    private CountCollision[] CountCollisions;
 
     protected void Start()
     {
+		String timeStamp = DateTime.Now.ToString("dd_MM_yy_HH_mm_ss");
+		Timer = GetComponent<Timer> ();
+        CountCollisions = FindObjectsOfType<CountCollision>();
         if (String.Compare(fileName, "") != 0)
         {
             Debug.Log(String.Format("Creation du fichier {0}. Début ", fileName));
             // Create an instance of StreamWriter to write text to a file.
-            stream = new StreamWriter(Application.dataPath + "/" + fileName);
+            stream = new StreamWriter(Application.dataPath + "/" + fileName + timeStamp + ".csv");
             // Add some text to the file.
             stream.Write("Fichier de Log : ");
-            stream.WriteLine("time;mouseX;mouseY;button");
+			stream.WriteLine("id;TimerCube1;TimerCube2;TimerCube3;Timer;DistanceCube1;DistanceCube2;DistanceCube3;WallCollision");
             stream.WriteLine("-------------------");
 
-            InvokeRepeating("writeLine", 0.1f, timeSample);
+            //InvokeRepeating("writeLine", 0.1f, timeSample);
         }
         else
         {
@@ -38,15 +44,20 @@ public class Log : MonoBehaviour
     protected void Update()
     {
         if (Input.GetMouseButtonDown(0))
-            stream.WriteLine(Time.time.ToString("00.00") + ";" + Input.mousePosition.x + ";" + Input.mousePosition.y + ";" + 1);
-        if (Input.GetMouseButtonDown(1))
-            stream.WriteLine(Time.time.ToString("00.00") + ";" + Input.mousePosition.x + ";" + Input.mousePosition.y + ";" + 2);
+        {
+            int SumCollisions = 0; 
+            foreach (CountCollision cc in CountCollisions)
+            {
+                SumCollisions += cc.countCollisions;
+            }
+            stream.WriteLine(Timer.IntermediateTime[0] + ";" + Timer.IntermediateTime[1] + ";" + Timer.IntermediateTime[2] + ";" + Timer.CumulativeTime[2] + ";" + SumCollisions);
+        }
     }
 
-    protected void writeLine()
+    /*protected void writeLine()
     {
         stream.WriteLine(Time.time.ToString("00.00") + ";" + Input.mousePosition.x + ";" + Input.mousePosition.y + ";" + 0); //mouseposition en pixel dans la fenetre Game (0,0) en bas à gauche
-    }
+    }*/
 
     protected void OnApplicationQuit()
     {
